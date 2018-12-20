2c9bb7822c1950d8c0b67fd7b562e760ae28a506
Diego Rosas <diego8242@gmail.com>
2018-12-20 13:58:10 -0600

    Se realizo el Problema 4

---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class Problema4
+    {
+        static void Main(string[] args)
+        {
+            //Se debe desarrollar un programa que pida el ingreso del precio de un articulo y la cantidad que lleva el cliente, 
+            //mostrar lo que debe pagar el comprador.
+
+            int num1, num2, num3;
+            int suma;
+
+            Console.Write("¿Que precio tiene los tenis? ");
+            num1 = Convert.ToInt32 (Console.ReadLine());
+
+            Console.Write("\n¿Que precio tiene la camisa? ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n¿Que precio tiene el pantalon? ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            suma = num1 + num2 + num3;
+
+            Console.WriteLine("\nEl total a pagar es de $" + suma);
+
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
