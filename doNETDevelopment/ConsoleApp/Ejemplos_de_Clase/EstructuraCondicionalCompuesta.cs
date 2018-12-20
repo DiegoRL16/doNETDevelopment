51b8bb498a8108a9436c48dc366ca943d8e60c64
Diego Rosas <diego8242@gmail.com>
2018-12-20 17:04:45 -0600

    Se realizo el Problema 5

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraCondicionalCompuesta.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraCondicionalCompuesta.cs
@@ -8,35 +8,35 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class EstructuraCondicionalCompuesta
     {
-        static void Main(string[] args)
-        {
-            //Programa que solicite ingresar 2 numeros distintos y muestre por pantalla el mayor de ellos
+        //static void Main(string[] args)
+        //{
+        //    //Programa que solicite ingresar 2 numeros distintos y muestre por pantalla el mayor de ellos
 
-            //Declarar varibles
-            int num1, num2;
+        //    //Declarar varibles
+        //    int num1, num2;
 
-            //Asignar un valor
-            Console.Write("Ingrese primer valor: ");
-            num1 = Convert.ToInt32 (Console.ReadLine());
+        //    //Asignar un valor
+        //    Console.Write("Ingrese primer valor: ");
+        //    num1 = Convert.ToInt32 (Console.ReadLine());
 
-            Console.Write("\nIngrese segundo valor: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("\nIngrese segundo valor: ");
+        //    num2 = Convert.ToInt32(Console.ReadLine());
 
-            //Validar cual de los dos es mayor
-            if (num1 > num2)
-            {
-                //(1) SALIDA: Verdadero
-                Console.WriteLine("\nEl numero mayor es: " + num1);
-            }
+        //    //Validar cual de los dos es mayor
+        //    if (num1 > num2)
+        //    {
+        //        //(1) SALIDA: Verdadero
+        //        Console.WriteLine("\nEl numero mayor es: " + num1);
+        //    }
 
-            else
-            {
-                //(2) SALIDA: Falso
-                Console.WriteLine("\nEl numero mayor es: " + num2);
-            }
+        //    else
+        //    {
+        //        //(2) SALIDA: Falso
+        //        Console.WriteLine("\nEl numero mayor es: " + num2);
+        //    }
 
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class Problema5
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa que lea por teclado 2 numeros, si el primero es mayor al segundo informar su suma y 
+            //resta, en caso contrario, informar el producto y la division del primero respecto al segundo.
+
+            int num1, num2;
+            int suma, resta, producto;
+            float division;
+
+            Console.Write("Introduce el primer numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nIntroduce el segundo numero: ");
+            num2 = Convert.ToInt32 (Console.ReadLine());
+
+            suma = num1 + num2;
+            resta = num1 - num2;
+
+            producto = num1 * num2;
+            division = num2 / num1;
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("\nLa suma es: " + suma);
+                Console.WriteLine("\nLa resta es: " + resta);               
+            }
+            else
+            {
+                Console.WriteLine("\nEl resultado de la multiplicacion: " + producto);
+                Console.WriteLine("\nEl resultado de la division es: " + division);
+            }
+
+            Console.WriteLine("\nPresione una teclas para salir: ");
+            Console.ReadKey();
+            
+        }
+    }
+}
