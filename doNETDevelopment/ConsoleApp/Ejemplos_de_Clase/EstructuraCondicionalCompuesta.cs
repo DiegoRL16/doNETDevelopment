3741a4ec14875fd602af78c5d467a2de3697cabc
Diego Rosas <diego8242@gmail.com>
2018-12-20 15:16:16 -0600

    Se realizo un ejercicio por consola aplicando estructura condicional compuesta

---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraCondicionalCompuesta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class EstructuraCondicionalCompuesta
+    {
+        static void Main(string[] args)
+        {
+            //Programa que solicite ingresar 2 numeros distintos y muestre por pantalla el mayor de ellos
+
+            //Declarar varibles
+            int num1, num2;
+
+            //Asignar un valor
+            Console.Write("Ingrese primer valor: ");
+            num1 = Convert.ToInt32 (Console.ReadLine());
+
+            Console.Write("\nIngrese segundo valor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Validar cual de los dos es mayor
+            if (num1 > num2)
+            {
+                //(1) SALIDA: Verdadero
+                Console.WriteLine("\nEl numero mayor es: " + num1);
+            }
+
+            else
+            {
+                //(2) SALIDA: Falso
+                Console.WriteLine("\nEl numero mayor es: " + num2);
+            }
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraCondicionalSimple.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraCondicionalSimple.cs
@@ -8,26 +8,26 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class EstructuraCondicionalSimple
     {
-        static void Main(string[] args)
-        {
-            //Programa para ingresar el sueldo de una persona, si supera los $3,000 pesos, 
-            //mostrar un mensaje en pantalla, indicando que debe abonar impuestos
+        //static void Main(string[] args)
+        //{
+        //    //Programa para ingresar el sueldo de una persona, si supera los $3,000 pesos, 
+        //    //mostrar un mensaje en pantalla, indicando que debe abonar impuestos
 
-            //Declarar las variables
-            float sueldo;
+        //    //Declarar las variables
+        //    float sueldo;
 
-            //Preguntar el sueldo de una persona
-            Console.Write("Ingrese el sueldo: ");
-            sueldo = float.Parse(Console.ReadLine());
+        //    //Preguntar el sueldo de una persona
+        //    Console.Write("Ingrese el sueldo: ");
+        //    sueldo = float.Parse(Console.ReadLine());
 
-            //Hacer la condicional para validar el sueldo
-            if (sueldo > 3000)
-            {
-                Console.WriteLine("\nEsta persona debe abonar impuestos");
-            }
+        //    //Hacer la condicional para validar el sueldo
+        //    if (sueldo > 3000)
+        //    {
+        //        Console.WriteLine("\nEsta persona debe abonar impuestos");
+        //    }
 
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
