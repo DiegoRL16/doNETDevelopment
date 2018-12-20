56507e8914c1780c8d0418387d72dcd72b8b8a4a
Diego Rosas <diego8242@gmail.com>
2018-12-20 14:59:22 -0600

    Se realizo un ejercicio de consola aplicando estructuras condicionales simples

---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraCondicionalSimple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class EstructuraCondicionalSimple
+    {
+        static void Main(string[] args)
+        {
+            //Programa para ingresar el sueldo de una persona, si supera los $3,000 pesos, 
+            //mostrar un mensaje en pantalla, indicando que debe abonar impuestos
+
+            //Declarar las variables
+            float sueldo;
+
+            //Preguntar el sueldo de una persona
+            Console.Write("Ingrese el sueldo: ");
+            sueldo = float.Parse(Console.ReadLine());
+
+            //Hacer la condicional para validar el sueldo
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("\nEsta persona debe abonar impuestos");
+            }
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema4.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema4.cs
@@ -8,30 +8,30 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema4
     {
-        static void Main(string[] args)
-        {
-            //Se debe desarrollar un programa que pida el ingreso del precio de un articulo y la cantidad que lleva el cliente, 
-            //mostrar lo que debe pagar el comprador.
+        //static void Main(string[] args)
+        //{
+        //    //Se debe desarrollar un programa que pida el ingreso del precio de un articulo y la cantidad que lleva el cliente, 
+        //    //mostrar lo que debe pagar el comprador.
 
-            int num1, num2, num3;
-            int suma;
+        //    int num1, num2, num3;
+        //    int suma;
 
-            Console.Write("¿Que precio tiene los tenis? ");
-            num1 = Convert.ToInt32 (Console.ReadLine());
+        //    Console.Write("¿Que precio tiene los tenis? ");
+        //    num1 = Convert.ToInt32 (Console.ReadLine());
 
-            Console.Write("\n¿Que precio tiene la camisa? ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("\n¿Que precio tiene la camisa? ");
+        //    num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("\n¿Que precio tiene el pantalon? ");
-            num3 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("\n¿Que precio tiene el pantalon? ");
+        //    num3 = Convert.ToInt32(Console.ReadLine());
 
-            suma = num1 + num2 + num3;
+        //    suma = num1 + num2 + num3;
 
-            Console.WriteLine("\nEl total a pagar es de $" + suma);
+        //    Console.WriteLine("\nEl total a pagar es de $" + suma);
 
 
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
