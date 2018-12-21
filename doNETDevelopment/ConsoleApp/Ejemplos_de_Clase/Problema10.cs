c1c387bcbd4e69e712f1a3af37d9959936fca6e2
Diego Rosas <diego8242@gmail.com>
2018-12-21 11:24:30 -0600

    Se realizo la Estructura de Opcion Multiple con el metodo de Switch

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema10.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema10.cs
@@ -8,42 +8,41 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema10
     {
-        static void Main(string[] args)
-        {
-            //Confeccionar un programa que permita cargar un numero entero positivo de hasta 3 cifras y muestre un mensaje indicando
-            // si tiene 1, 2 o 3 cifras. Mostrar un mensaje de error si el numero de cifras es mayor.
+        //static void Main(string[] args)
+        //{
+        //    //Confeccionar un programa que permita cargar un numero entero positivo de hasta 3 cifras y muestre un mensaje indicando
+        //    // si tiene 1, 2 o 3 cifras. Mostrar un mensaje de error si el numero de cifras es mayor.
 
-            int num;
+        //    int num;
 
-            Console.Write("Ingrese un numero: ");
-            num = Convert.ToInt32 (Console.ReadLine());
+        //    Console.Write("Ingrese un numero: ");
+        //    num = Convert.ToInt32 (Console.ReadLine());
 
-            if (num > 99 && num <= 999)
-            {
-                Console.WriteLine("\nEl numero tiene 3 cifras!");
-            }
-            else
-            {
-                if(num > 9 && num <= 99)
-                {
-                    Console.WriteLine("\nEl numero tiene 2 cifras!");
-                }
-                else
-                {
-                    if(num < 10)
-                    {
-                        Console.WriteLine("\nEl numero tiene 1 cifra!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nError! Este numero es nulo");
-                    }
-                }
-            }
+        //    if (num > 99 && num <= 999)
+        //    {
+        //        Console.WriteLine("\nEl numero tiene 3 cifras!");
+        //    }
+        //    else
+        //    {
+        //        if(num > 9 && num <= 99)
+        //        {
+        //            Console.WriteLine("\nEl numero tiene 2 cifras!");
+        //        }
+        //        else
+        //        {
+        //            if(num < 10)
+        //            {
+        //                Console.WriteLine("\nEl numero tiene 1 cifra!");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("\nError! Este numero es nulo");
+        //            }
+        //        }
+        //    }
 
-
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
