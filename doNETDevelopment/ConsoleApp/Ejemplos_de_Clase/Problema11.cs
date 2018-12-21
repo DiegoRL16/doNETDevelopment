8073dbcf1ecaaa936733457c8e116972c4e971a1
Diego Rosas <diego8242@gmail.com>
2018-12-21 12:18:39 -0600

    Se resolvio el Problema 12

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema11.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema11.cs
@@ -8,45 +8,45 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema11
     {
-        static void Main(string[] args)
-        {
-            //Programa que pida un numero del 1 al 7 y diga el dia de la semana correspondiente
+        //static void Main(string[] args)
+        //{
+        //    //Programa que pida un numero del 1 al 7 y diga el dia de la semana correspondiente
 
-            int num;
+        //    int num;
 
-            Console.Write("Ingrese un numero (1-7): ");
-            num = Convert.ToInt32 (Console.ReadLine());
+        //    Console.Write("Ingrese un numero (1-7): ");
+        //    num = Convert.ToInt32 (Console.ReadLine());
 
-            switch (num)
-            {
-                case 1:
-                    Console.WriteLine("\nLunes");
-                    break;
-                case 2:
-                    Console.WriteLine("\nMartes");
-                    break;
-                case 3:
-                    Console.WriteLine("\nMiercoles");
-                    break;
-                case 4:
-                    Console.WriteLine("\nJueves");
-                    break;
-                case 5:
-                    Console.WriteLine("\nViernes");
-                    break;
-                case 6:
-                    Console.WriteLine("\nSabado");
-                    break;
-                case 7:
-                    Console.WriteLine("\nDomingo");
-                    break;
-                default:
-                    Console.WriteLine("\nIngrese un numero valido");
-                    break;
-            }
+        //    switch (num)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("\nLunes");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("\nMartes");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("\nMiercoles");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("\nJueves");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("\nViernes");
+        //            break;
+        //        case 6:
+        //            Console.WriteLine("\nSabado");
+        //            break;
+        //        case 7:
+        //            Console.WriteLine("\nDomingo");
+        //            break;
+        //        default:
+        //            Console.WriteLine("\nIngrese un numero valido");
+        //            break;
+        //    }
 
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema12.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class Problema12
+    {
+        static void Main(string[] args)
+        {
+            /// Programa que muestre un menu de opciones como el siguiente:
+            ///1.Suma
+            ///2.Resta
+            ///3.Multiplicacion
+            ///4.Division
+            ///Y haga la operacion correspondiente a la opcion elegida, tomando como datos de entrada 2 numeros enteros.
+
+            int num1, num2;
+            int num;
+            int suma, resta, multiplicacion, division;
+
+            Console.Write("Introduce el primer numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nIntroduce el segundo numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine
+                ("\n\n1. Suma" +
+                "\n2. Resta" +
+                "\n3. Multiplicacion" +
+                "\n4. Division");
+
+            Console.Write("\n\nElige una opcion del menu: ");
+            num = Convert.ToInt32 (Console.ReadLine());
+
+            suma = num1 + num2;
+            resta = num1 - num2;
+            multiplicacion = num1 * num2;
+            division = num1 / num2;
+
+            switch (num)
+            {
+                case 1:
+
+                    Console.WriteLine("\nSuma: ");
+
+                    Console.WriteLine("\nEl resultado de la Suma es: " + suma);
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("\nResta: ");
+
+                    Console.WriteLine("\nEl resultado de la Resta es: " + resta);
+
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("\nMultiplicacion: ");
+
+                    Console.WriteLine("\nEl resultado de la Multiplicacion es: " + multiplicacion);
+
+                    break;
+
+                case 4:
+
+                    Console.WriteLine("\nDivision: ");
+
+                    Console.WriteLine("\nEl resultado de la Division es: " + division);
+
+                    break;
+
+                default:
+                    Console.WriteLine("\nIntroduce un valor valido bye...");
+                    break;
+                    
+            }
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
