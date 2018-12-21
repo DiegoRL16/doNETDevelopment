84b78be9895859bfa65d029ca64d9788a9574366
Diego Rosas <diego8242@gmail.com>
2018-12-20 18:41:05 -0600

    Se realizo el Problema 7

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema6.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema6.cs
@@ -8,36 +8,36 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema6
     {
-        static void Main(string[] args)
-        {
-            //Se ingresan 3 notas de una alumno si el promedio es mayor o igual a 7 mostrar un mensaje que diga promocionado.ç
+        //static void Main(string[] args)
+        //{
+        //    //Se ingresan 3 notas de una alumno si el promedio es mayor o igual a 7 mostrar un mensaje que diga promocionado.ç
 
-            int num1, num2, num3;
-            int suma, promedio;
+        //    int num1, num2, num3;
+        //    int suma, promedio;
 
-            Console.Write("Ingresa la primer tarea: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Ingresa la primer tarea: ");
+        //    num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("\nIngresa la segunda tarea: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("\nIngresa la segunda tarea: ");
+        //    num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("\nIngresa la tercer tarea: ");
-            num3 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("\nIngresa la tercer tarea: ");
+        //    num3 = Convert.ToInt32(Console.ReadLine());
 
-            suma = num1 + num2 + num3;
-            promedio = suma / 3;
+        //    suma = num1 + num2 + num3;
+        //    promedio = suma / 3;
 
-            if (promedio >= 7)
-            {
-                Console.WriteLine("\nFelicidades, Aprobaste Perro!");
-            }
-            else
-            {
-                Console.WriteLine("\nVales vrga we, alch te mmas, pinche burro");
-            }
+        //    if (promedio >= 7)
+        //    {
+        //        Console.WriteLine("\nFelicidades, Aprobaste Perro!");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("\nVales vrga we, alch te mmas, pinche burro");
+        //    }
             
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class Problema7
+    {
+        static void Main(string[] args)
+        {
+            //Se ingresa por teclado un numero positivo de uno o dos digitos (1 - 99) mostrar un mensaje indicando si el numero tiene 1 o 2 digitos.
+
+            int num1;
+
+            Console.Write("Introduzca un numero entre 1-99: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > 9 )
+            {
+                Console.WriteLine("\nEste valor contiene 2 digitos");
+            }
+            else
+            {
+                Console.WriteLine("\nEste valor contiene 1 digito");
+            }
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
