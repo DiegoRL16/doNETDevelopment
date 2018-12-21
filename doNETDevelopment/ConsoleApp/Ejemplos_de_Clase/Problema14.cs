0f27b6d1d19136bf3150253935965d7a83d23f21
Diego Rosas <diego8242@gmail.com>
2018-12-21 14:32:36 -0600

    Se resolvio el Problema 15

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema14.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema14.cs
@@ -8,35 +8,35 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema14
     {
-        static void Main(string[] args)
-        {
-            //Se ingresan un conjunto de N alturas de personas por teclado. Mostrar la altura promedio de las personas
+        //static void Main(string[] args)
+        //{
+        //    //Se ingresan un conjunto de N alturas de personas por teclado. Mostrar la altura promedio de las personas
 
-            int n, x;
-            float altura, suma, promedio;
+        //    int n, x;
+        //    float altura, suma, promedio;
 
-            Console.Write("Cuantas personas hay? ");
-            n = Convert.ToInt32 (Console.ReadLine());
+        //    Console.Write("Cuantas personas hay? ");
+        //    n = Convert.ToInt32 (Console.ReadLine());
 
-            x = 1;
-            suma = 0;
+        //    x = 1;
+        //    suma = 0;
 
-            while(x <= n)
-            {
-                Console.Write("Ingrese la altura: ");
-                altura = Convert.ToInt32 (Console.ReadLine());
+        //    while(x <= n)
+        //    {
+        //        Console.Write("Ingrese la altura: ");
+        //        altura = Convert.ToInt32 (Console.ReadLine());
 
-                suma = suma + altura;
+        //        suma = suma + altura;
                 
-                x = x + 1;
-            }
+        //        x = x + 1;
+        //    }
 
-            promedio = suma / n;
-            Console.WriteLine("\nLa altura promedio es: " + promedio);
+        //    promedio = suma / n;
+        //    Console.WriteLine("\nLa altura promedio es: " + promedio);
 
 
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class Problema15
+    {
+        static void Main(string[] args)
+        {
+            //Programa que imprima 25 terminos de la serie 11
+
+            int x, n, a;
+
+            x = 1;
+            n = 11;
+
+            while(x <= 25)
+            {
+                Console.WriteLine(n);
+                Console.WriteLine(" - ");
+                x = x + 1;
+                n = n + 11;
+            }
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
