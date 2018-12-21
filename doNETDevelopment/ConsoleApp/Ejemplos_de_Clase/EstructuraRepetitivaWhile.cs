46269c6f290be01e4087953229b208ba31f7b099
Diego Rosas <diego8242@gmail.com>
2018-12-21 12:52:05 -0600

    Se realizo la Estructura Repetitiva While

---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraRepetitivaWhile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class EstructuraRepetitivaWhile
+    {
+        static void Main(string[] args)
+        {
+            //Programa que imprima en pantalla los numeros del al 100
+
+            //Declarar las variables
+            int x;
+
+            //Asignar valor a las variables
+            x = 1;
+
+            //Repeticion y la impresion del numero
+            while (x <= 100)
+            {
+                Console.Write(x); //Imprimimos el valor de la x
+
+                if (x != 100)
+                {
+                    Console.Write(" - ");
+                }
+
+                x = x + 1; //Incrementando la x en 1 por cada vuelta
+            }
+
+            Console.WriteLine("\n Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
