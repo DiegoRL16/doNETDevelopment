e5abca723bbb285516ccb54b9f0359b31cd0052d
Diego Rosas <diego8242@gmail.com>
2018-12-21 11:39:08 -0600

    Se resolvio el Problema 11

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraOpcionMultiple.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/EstructuraOpcionMultiple.cs
@@ -8,46 +8,46 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class EstructuraOpcionMultiple
     {
-        static void Main(string[] args)
-        {
-            //Programa que reciba numero del 1 al 5, y devolver un String con el nombre del numero (Mostrar en pantalla)
+        //static void Main(string[] args)
+        //{
+        //    //Programa que reciba numero del 1 al 5, y devolver un String con el nombre del numero (Mostrar en pantalla)
 
-            //Declarar variables
-            int numero;
+        //    //Declarar variables
+        //    int numero;
 
-            //Asignar un valor a las variables
-            Console.Write("Ingrese un numero (1-5): ");
-            numero = Convert.ToInt32 (Console.ReadLine());
+        //    //Asignar un valor a las variables
+        //    Console.Write("Ingrese un numero (1-5): ");
+        //    numero = Convert.ToInt32 (Console.ReadLine());
 
-            //Aplicar opcion multiple
-            switch(numero)
-            {
-                //Casos con cada una de las opciones
-                case 1:
-                    Console.WriteLine("\nUno");
-                    break; //Detener el proceso
+        //    //Aplicar opcion multiple
+        //    switch(numero)
+        //    {
+        //        //Casos con cada una de las opciones
+        //        case 1:
+        //            Console.WriteLine("\nUno");
+        //            break; //Detener el proceso
 
-                case 2:
-                    Console.WriteLine("\nDos");
-                    break;
-                case 3:
-                    Console.WriteLine("\nTres");
-                    break;
-                case 4:
-                    Console.WriteLine("\nCuatro");
-                    break;
-                case 5:
-                    Console.WriteLine("\nCinco");
-                    break;
+        //        case 2:
+        //            Console.WriteLine("\nDos");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("\nTres");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("\nCuatro");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("\nCinco");
+        //            break;
 
-                    //En caso de no seleccionar un numero del 1 al 5, mostrar este mensaje
-                default:
-                    Console.WriteLine("\nIngrese un valor valido bye");
-                    break;
-            }
+        //            //En caso de no seleccionar un numero del 1 al 5, mostrar este mensaje
+        //        default:
+        //            Console.WriteLine("\nIngrese un valor valido bye");
+        //            break;
+        //    }
             
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
