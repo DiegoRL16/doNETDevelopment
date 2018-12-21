1570969396cc7844ec62127e35ce6774aa8671fb
Diego Rosas <diego8242@gmail.com>
2018-12-21 11:00:53 -0600

    Se resolvio el Problema 10

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema9.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema9.cs
@@ -8,35 +8,35 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema9
     {
-        static void Main(string[] args)
-        {
-            //Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el numero es positvo, nulo o negativo.
+        //static void Main(string[] args)
+        //{
+        //    //Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el numero es positvo, nulo o negativo.
 
-            int num;
-            int neg = 0;
-            int nulo = 1000;
+        //    int num;
+        //    int neg = 0;
+        //    int nulo = 1000;
 
-            Console.Write("Ingresa un valor entero: ");
-            num = Convert.ToInt32 (Console.ReadLine());
+        //    Console.Write("Ingresa un valor entero: ");
+        //    num = Convert.ToInt32 (Console.ReadLine());
 
-            if (num > neg && num < nulo )
-            {
-                Console.WriteLine("\nEste es un numero positivo!");
-            }
-            else
-            {
-                if(num < neg)
-                {
-                    Console.WriteLine("\nEste numero es negativo!");
-                }
-                else
-                {
-                    Console.WriteLine("\nEste numero es nulo!");
-                }
-            }
+        //    if (num > neg && num < nulo )
+        //    {
+        //        Console.WriteLine("\nEste es un numero positivo!");
+        //    }
+        //    else
+        //    {
+        //        if(num < neg)
+        //        {
+        //            Console.WriteLine("\nEste numero es negativo!");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("\nEste numero es nulo!");
+        //        }
+        //    }
 
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
