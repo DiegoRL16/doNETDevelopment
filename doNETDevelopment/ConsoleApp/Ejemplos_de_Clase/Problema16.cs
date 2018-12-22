7041d86aac2fa9b0332a636b7105630f75874ea6
Diego Rosas <diego8242@gmail.com>
2018-12-21 18:37:55 -0600

    Se resolvio el Problema 17

--- doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema16.cs
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema16.cs
@@ -8,30 +8,30 @@ namespace ConsoleApp.Ejemplos_de_Clase
 {
     class Problema16
     {
-        static void Main(string[] args)
-        {
-            //Programa que solicite la carga de 10 numeros e imprima la suma de los ultimos 5 valores
+        //static void Main(string[] args)
+        //{
+        //    //Programa que solicite la carga de 10 numeros e imprima la suma de los ultimos 5 valores
 
-            int suma, valor;
+        //    int suma, valor;
 
-            suma = 0;
-            valor = 0;
+        //    suma = 0;
+        //    valor = 0;
 
-            for(int i = 1; i <=10; i++)
-            {
-                Console.Write("Introduce un numero: ");
-                valor = Convert.ToInt32 (Console.ReadLine());
-                if (i > 5)
-                {
-                    suma = suma + valor;
-                }
+        //    for(int i = 1; i <=10; i++)
+        //    {
+        //        Console.Write("Introduce un numero: ");
+        //        valor = Convert.ToInt32 (Console.ReadLine());
+        //        if (i > 5)
+        //        {
+        //            suma = suma + valor;
+        //        }
 
-            }
+        //    }
 
-            Console.WriteLine("\nLa suma de los ultimos 5 numeros es: " + suma);
+        //    Console.WriteLine("\nLa suma de los ultimos 5 numeros es: " + suma);
             
-            Console.WriteLine("\nPresione una tecla para salir");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("\nPresione una tecla para salir");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ doNETDevelopment/ConsoleApp/Ejemplos_de_Clase/Problema17.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Ejemplos_de_Clase
+{
+    class Problema17
+    {
+        static void Main(string[] args)
+        {
+            //Programa que muestre la tabla de multiplicar del 5 (del 5 al 50)
+
+            int num, resultado;
+
+            Console.Write("Ingresa el valor a multiplicar: ");
+            num = int.Parse (Console.ReadLine());
+
+            for (int i = 1; i <= 10; i ++)
+            {
+                resultado = num * i;
+                Console.WriteLine(num + " * " + i + " = " + resultado);
+            }
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
